9c5160331e74be716cbf801419453589c84ce536
9Laz1e <9Laz1e@mail.ru>
2025-01-07 18:19:52 +0500

    fix

--- pribor_tashkent/settings.py
+++ pribor_tashkent/settings.py
@@ -122,12 +122,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = '/var/www/app/static/'  # Должно совпадать с конфигурацией в Nginx
 
-
-MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/var/www/app/media/'
 
 
 # Default primary key field type
