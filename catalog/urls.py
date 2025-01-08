bbb2e88622a1dc9150f2f8691a2aedc659591d6b
9Laz1e <9Laz1e@mail.ru>
2025-01-08 18:24:02 +0500

    update

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,5 +4,6 @@ from .views import *
 
 urlpatterns = [
     path('categories-get/', CategoryView.as_view(), name='categories-get'),
+    path('products/category/<int:category_id>/', ProductsByCategoryView.as_view(), name='products-get-by-category'),
     path('products-get/', ProductView.as_view(), name='products-get')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -4,6 +4,8 @@ from .models import Product, Category
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.shortcuts import get_object_or_404
+
 
 class CategoryView(APIView):
     serializer_class = CategorySerializer
@@ -20,4 +22,19 @@ class ProductView(APIView):
     def get(self, request, *args, **kwargs):
         products = Product.objects.all()
         serializer = ProductSerializer(products, context={'request':request}, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+
+
+
+class ProductsByCategoryView(APIView):
+    serializer_class = ProductSerializer
+
+    def get(self, request, category_id, *args, **kwargs):
+        # Проверяем, существует ли категория
+        category = get_object_or_404(Category, id=category_id)
+        # Получаем все товары для указанной категории
+        products = Product.objects.filter(category=category)
+        # Сериализуем данные
+        serializer = ProductSerializer(products, context={'request': request}, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
