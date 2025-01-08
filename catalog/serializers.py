463d8dcceceea9fb20a8af021deecd9810b86daf
9Laz1e <9Laz1e@mail.ru>
2025-01-08 19:39:07 +0500

    upd

--- catalog/serializers.py
+++ catalog/serializers.py
@@ -1,15 +1,16 @@
 from rest_framework import serializers
 from .models import Product, Category
 
-
 class CategorySerializer(serializers.ModelSerializer):
-    class Meta():
+    class Meta:
         model = Category
         fields = ['id', 'name']
 
 class ProductSerializer(serializers.ModelSerializer):
-    class Meta():
+    category_name = CategorySerializer()  # Вложенный сериализатор для отображения объекта категории
+
+    class Meta:
         model = Product
-        fields = ['id', 'category', 'name', 'image', 'description', 'cost']
+        fields = ['id', 'category_name', 'name', 'image', 'description', 'cost']
 
 
